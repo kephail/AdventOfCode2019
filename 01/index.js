e63acb5b879240d5359570465353940ff3d03fb2
Peter Jones <kephail@me.com>
2019-12-10 13:25:12 +0000

    calculateRequiredFuel logic and passing tests

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,9 @@
 function calculateRequiredFuel(module) {
-  return module;
+  /*
+    Fuel required to launch a given module is based on its mass. 
+    Specifically, to find the fuel required for a module, take its mass, 
+    divide by three, round down, and subtract 2.
+    */
+  return Math.floor(module / 3) - 2;
 }
 module.exports = calculateRequiredFuel;
