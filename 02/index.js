6a469aec665f8e78a6203dd84621ac1ec17a20c0
Peter Jones <kephail@me.com>
2019-12-15 21:40:26 +0000

    Passing tests - could definitely be cleaner :poop:

--- 02/index.js
+++ 02/index.js
@@ -1,5 +1,16 @@
-const interpretIntcode = () => {
-  return "";
+const interpretIntcode = program => {
+  const steps = program.split(",").map(step => parseInt(step));
+  for (let i = 0; i < steps.length; i += 4) {
+    const opcode = steps[i];
+    if (opcode === 99) return steps.toString();
+    if (opcode === 1) {
+      steps[steps[i + 3]] = steps[steps[i + 1]] + steps[steps[i + 2]];
+    }
+    if (opcode === 2) {
+      steps[steps[i + 3]] = steps[steps[i + 1]] * steps[steps[i + 2]];
+    }
+  }
+  return steps.toString();
 };
 
 module.exports = {
